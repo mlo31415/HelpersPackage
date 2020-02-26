429aaca1be545ee913001eee36e0af6d3beb6a02
mlo31415 <mlo@baskerville.org>
2020-02-26 12:59:30 -0500

    Update Log for deferred creation of log files

--- Log.py
+++ Log.py
@@ -1,4 +1,5 @@
 import os
+import io
 
 import HelpersPackage
 #from HelpersPackage import MessageBox
@@ -19,10 +20,28 @@ import HelpersPackage
 # If isError is set also print it to the error file.
 def Log(text: str, isError: bool=False, noNewLine: bool=False) -> None:
     global g_logFile
-    global g_errorFile
+    global g_logErrorFile
     global g_logHeader
     global g_logErrorHeader
 
+    # We don't actually create the log files until there's something written to them
+    # LogOpen stores the names of the output files in g_logFile and g_errorFile.
+    # (If those globals don't contain strings, then they were probably never initialized.)
+    # Now, if the file is needed, we open the files and store the file handle in that global instead.
+    if g_logFile is not None and isinstance(g_logFile, str):
+        try:
+            g_logFile=open(g_logFile, "w+")
+        except Exception as e:
+            HelpersPackage.MessageBox("Exception in LogOpen("+g_logFile+")  Exception="+str(e))
+
+    if isError and g_logErrorFile is not None and isinstance(g_logErrorFile, str):
+        try:
+            g_logErrorFile=open(g_logErrorFile, "w+", buffering=1)
+        except Exception as e:
+            HelpersPackage.MessageBox("Exception in LogOpen("+g_logFile+")  Exception="+str(e))
+
+    # We allow the user to specify that the file is not terminated with a new line, allowing several log calls
+    # to go to the same output line.
     newlinechar="\n"
     if noNewLine:
         newlinechar=" "
@@ -36,10 +55,10 @@ def Log(text: str, isError: bool=False, noNewLine: bool=False) -> None:
     if isError:
         # If this is an error entry and is the first error entry for this header, print the header and then clear it so it's not printed again
         if g_logErrorHeader is not None:
-            print("----\n"+g_logErrorHeader, file=g_errorFile)
+            print("----\n"+g_logErrorHeader, file=g_logErrorFile)
         g_logErrorHeader=None
 
-    if g_logFile is None and g_errorFile is None:
+    if g_logFile is None and g_logErrorFile is None:
         print("*** Log() called prior to call to LogOpen()", end=newlinechar)
         print("*** text="+text, end=newlinechar)
 
@@ -47,7 +66,7 @@ def Log(text: str, isError: bool=False, noNewLine: bool=False) -> None:
     print(text, end=newlinechar)
     print(text, file=g_logFile, end=newlinechar)
     if isError:
-        print(text, file=g_errorFile, end=newlinechar)
+        print(text, file=g_logErrorFile, end=newlinechar)
         LogFlush()
 
 
@@ -66,15 +85,13 @@ def LogSetHeader(name: str) -> None:
 
 
 #=============================================================================
+# This really doesn't do the open, but just caches the filenames.  They'll be opened by Log() only if needed.
 def LogOpen(logfilename: str, errorfilename: str) -> None:
     global g_logFile
-    try:
-        g_logFile=open(logfilename, "w+")
-    except Exception as e:
-        HelpersPackage.MessageBox("Exception in LogOpen("+logfilename+")  Exception="+str(e))
+    g_logFile=logfilename
 
-    global g_errorFile
-    g_errorFile=open(errorfilename, "w+", buffering=1)
+    global g_logErrorFile
+    g_logErrorFile=errorfilename
 
     global g_logHeader
     g_logHeader=None
@@ -86,19 +103,28 @@ def LogOpen(logfilename: str, errorfilename: str) -> None:
 
 #=============================================================================
 def LogFlush() -> None:
-    g_logFile.flush()
-    g_errorFile.flush()
+    global g_logFile
+    if g_logFile is not None and isinstance(g_logFile, io.TextIOWrapper):
+        g_logFile.flush()
+    global g_logErrorFile
+    if g_logErrorFile is not None and isinstance(g_logErrorFile, io.TextIOWrapper):
+        g_logErrorFile.flush()
 
 
 #=============================================================================
+# Needed only if you want to close the log files before program termination.
 def LogClose() -> None:
     global g_logFile
-    g_logFile.close()
-    global g_errorFile
-    g_errorFile.close()
+    if g_logFile is not None and isinstance(g_logFile, io.TextIOWrapper):
+        g_logFile.close()
+    global g_logErrorFile
+    if g_logErrorFile is not None and isinstance(g_logErrorFile, io.TextIOWrapper):
+        g_logErrorFile.close()
 
 
 #=============================================================================
+# Check to see if a filename exists.  If it doesn't, log the fact and raise an exception
+# If it does exiats, return doing notning.
 def LogFailureAndRaiseIfMissing(fname: str) -> None:
     if not os.path.exists(fname):
         Log("Fatal error: Can't find "+fname, isError=True)

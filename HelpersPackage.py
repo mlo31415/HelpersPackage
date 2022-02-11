f9b9f53cdc252039454f80397c25be884071d267
mlo31415 <mlo@baskerville.org>
2022-02-11 11:17:32 -0500

    REname argument to avoid showing message

--- HelpersPackage.py
+++ HelpersPackage.py
@@ -308,8 +308,8 @@ def RemoveHTTP(input: str) -> str:
 
 #=====================================================================================
 # Remove the accents, unlats, etc from characters
-def RemoveAccents(input: str) -> str:
-    nfkd_form = unicodedata.normalize('NFKD', input)
+def RemoveAccents(s: str) -> str:
+    nfkd_form = unicodedata.normalize('NFKD', s)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 

9762a52b5a17b537627eb9e7c9899ca07df272be
mlo31415 <mlo@baskerville.org>
2022-03-27 15:49:00 -0400

    Add new function to remove UTF-8 non-breaking spaces

--- HelpersPackage.py
+++ HelpersPackage.py
@@ -369,6 +369,11 @@ def RemoveAllHTMLTags2(s: str) -> str:
 
 
 #=====================================================================================
+# Turn some whitespace escape characters into spaces
+def RemoveFunnyWhitespace(s: str) -> str:
+    return s.replace("\xc2\xa0", " ").replace(u"\u00A0", " ")
+
+#=====================================================================================
 # Change"&nbsp;" to space
 def ChangeNBSPToSpace(s: Optional[str]) -> Union[None, str, list[str]]:
     if s is None:

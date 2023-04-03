a0f8487c411b29046f4873380b13202b601307d2
mlo31415 <mlo@baskerville.org>
2023-04-03 19:56:08 -0400

    Change YesNoMaybe()'s response to an unknown input to 'unknown'

--- HelpersPackage.py
+++ HelpersPackage.py
@@ -1158,14 +1158,14 @@ def SortTitle(inputstring: str) -> str:
 
 # ==========================================================
 # Take an input string and check for "yes" or "no" (case insenstive, abbreviations allowed.
-# Return "yesy" or "no" or "" depending on what's found
+# Return "yesy" or "no" or "unknown" depending on what's found
 def YesNoMaybe(s: str) -> str:
     s=s.strip().lower()
     if s == "yes" or s == "y" or s == "true":
         return "yes"
     if s == "no" or s == "n" or s == "false":
         return "no"
-    return ""
+    return "unknown"
 
 
 # ==========================================================

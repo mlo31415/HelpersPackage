b840b507ecc1e044ab7f3207111b8e350fdf720d
mlo31415 <mlo@baskerville.org>
2024-11-15 09:42:27 -0500

    Add the isURL flag to HtmlEscapesToUnicode and UnicodeToHtmlEscapes to deal with %23, %26, and %20 in URLs

--- HtmlHelpersPackage.py
+++ HtmlHelpersPackage.py
@@ -2,9 +2,13 @@ import bs4
 from bs4 import dammit
 
 #=====================================================================================
-def HtmlEscapesToUnicode(s: str) -> str:
-    return str(bs4.BeautifulSoup(s)).strip()
+def HtmlEscapesToUnicode(s: str, isURL=False) -> str:
+    if isURL:
+        s=s.replace("%23", "#").replace( "%26", "&").replace( "%20", " ")
+    return str(bs4.BeautifulSoup(s, features="html.parser")).strip()
 
 
-def UnicodeToHtmlEscapes(s: str) -> str:
+def UnicodeToHtmlEscapes(s: str, isURL=False) -> str:
+    if isURL:
+        s=s.replace("#", "%23").replace("&", "%26").replace( " ", "%20")
     return bs4.dammit.EntitySubstitution.substitute_html(s)

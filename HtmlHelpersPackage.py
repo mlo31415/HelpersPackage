68f2feb9dc7d0db22dd9bc9ca3ea6f79e800aef5
mlo31415 <mlo@baskerville.org>
2025-01-09 13:22:46 -0500

    Tweak HtmlEscapesToUnicode() and UnicodeToHtmlEscapes() to handle #, & and space better

--- HtmlHelpersPackage.py
+++ HtmlHelpersPackage.py
@@ -5,10 +5,22 @@ from bs4 import dammit
 def HtmlEscapesToUnicode(s: str, isURL=False) -> str:
     if isURL:
         s=s.replace("%23", "#").replace( "%26", "&").replace( "%20", " ")
-    return str(bs4.BeautifulSoup(s, features="html.parser")).strip()
-
+    s=str(bs4.BeautifulSoup(s, features="html.parser")).strip()
+    if isURL:
+        s=s.replace("%23", "#").replace( "%26", "&").replace( "%20", " ")
+    return s
 
 def UnicodeToHtmlEscapes(s: str, isURL=False) -> str:
     if isURL:
         s=s.replace("#", "%23").replace("&", "%26").replace( " ", "%20")
-    return bs4.dammit.EntitySubstitution.substitute_html(s)
+    s=bs4.dammit.EntitySubstitution.substitute_html(s)
+    if isURL:
+        s=s.replace("#", "%23").replace("&", "%26").replace( " ", "%20")
+    return s
+
+#=====================================================================================
+def ConvertHTMLEscapes(s: str) -> str:
+    s=s.replace("&amp;", "&")
+    s=s.replace("%23", "#").replace( "%26", "&").replace( "%20", " ")
+    s=s.replace("&gt;", ">").replace( "&lt;", "<").replace("&nbsp;", " ")
+    return s

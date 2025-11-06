f0b35123db86b8647cf93e3a375bb72bb472fc5c
mlo31415 <mlo@baskerville.org>
2025-11-06 09:36:10 -0500

    REmove obsolete comment

--- FanacFanzinesHelpers.py
+++ FanacFanzinesHelpers.py
@@ -3,7 +3,6 @@ from Log import Log
 
 def ReadClassicFanzinesTable(html: str) -> list[str]|None:
     # Parse the HTML looking for the classic fanzines table
-    # (Note that this used to be done using BeautifulSoup, but it was very slow.)
     m=re.search(r"<table[^>]*sortable\">(.*)$", html, flags=re.DOTALL|re.IGNORECASE)
     if m is None:
         Log("ReadClassicFanzinesTable: Could not find sortable table.")

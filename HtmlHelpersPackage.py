dfc225701bd3788f3fdab7d34a0ba4f4d8d5e165
mlo31415 <mlo@baskerville.org>
2026-01-27 21:14:42 -0500

    Add typehints

--- HtmlHelpersPackage.py
+++ HtmlHelpersPackage.py
@@ -10,7 +10,7 @@ def HtmlEscapesToUnicode(s: str, isURL=False) -> str:
         s=s.replace("%23", "#").replace( "%26", "&").replace( "%20", " ")
     return s
 
-def UnicodeToHtmlEscapes(s: str, isURL=False) -> str:
+def UnicodeToHtmlEscapes(s: str, isURL: bool=False) -> str:
     if isURL:
         s=s.replace("#", "%23").replace("&", "%26").replace( " ", "%20")
     s=bs4.dammit.EntitySubstitution.substitute_html(s)
